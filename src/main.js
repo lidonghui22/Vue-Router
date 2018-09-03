21ffe602e2cfee562149c8ef97af9441754de6a0
lidonghui22 <840776160@qq.com>
2018-09-04 07:18:30 +0800

    路由钩子函数

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+    console.log(to);
+    console.log(from);
+    next();
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +18,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
